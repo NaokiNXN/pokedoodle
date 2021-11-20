04201062888c9e5518b83888d47319c2f8d9377d
NaokiNXN <nxn1@outlook.com>
2021-11-20 03:02:54 +0200

    incomplete DB stuff

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const Sequelize = require('sequelize');
 
 /**
  * Intents and token setup and assignment
@@ -23,7 +24,70 @@ client.events = new Discord.Collection();
 client.cooldowns = new Set();
 
 
-//DB SETUP
+/**
+ * This section creates a relational DB using serialize and sqlite
+ * we then attach it to the client for easy access elsewhere.
+ */
+
+const sequelize = new Sequelize('database', 'user', 'password', {
+    host: 'localhost',
+    dialect: 'sqlite',
+    logging: false,
+    storage: 'database.sqlite',
+});
+
+const Tags = sequelize.define('tags', {
+    name: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false,
+    },
+    dexNumber: {
+        type: Sequelize.INTEGER,
+        defaultValue: 0,
+        allowNull: false,
+    },
+    dexEntry: {
+        type: Sequelize.TEXT,
+        allowNull: false
+    },
+    type: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    height: {
+        type: Sequelize.FLOAT,
+        defaultValue: 0,
+    },
+    weight: {
+        type: Sequelize.FLOAT,
+        defaultValue: 0,
+    },
+    hp: {
+        type: Sequelize.INTEGER,
+        defaultValue: 0,
+    },
+    atk: {
+        type: Sequelize.INTEGER,
+        defaultValue: 0,
+    },
+    def: {
+        type: Sequelize.INTEGER,
+        defaultValue: 0,
+    },
+    specialAtk: {
+        type: Sequelize.INTEGER,
+        defaultValue: 0,
+    },
+    specialDef: {
+        type: Sequelize.INTEGER,
+        defaultValue: 0,
+    },
+    speed: {
+        type: Sequelize.INTEGER,
+        defaultValue: 0,
+    },
+})
 
 
 /**
