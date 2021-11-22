c3eacbaf6de3a53888243bb701770e0070fa44b1
NaokiNXN <nxn1@outlook.com>
2021-11-22 18:41:45 +0200

    Guess that pokemon complete

--- commands/guess.js
+++ commands/guess.js
@@ -29,27 +29,49 @@ module.exports = {
             const doodleBuffer = pokemon.get('doodle');
 
             const doodle = await Jimp.read(doodleBuffer).then(async image => {
-                return await image.resize(420,380);
+                return await image.resize(420, 380);
             }).catch(err => {
                 console.log(err);
                 return interaction.followUp('An error occured when resizing the image please log with bot author');
             });
 
             const guess = await Jimp.read('/usr/src/bot/commands/assets/wtp.png').then(async image => {
-                return await image.composite(doodle.brightness(-1), 138, 134);
+                const guess = await image.clone().composite(doodle.clone().brightness(-1), 138, 134);
+                const trueImage = await image.composite(doodle, 138, 134);
+                return [guess, trueImage];
             }).catch(err => {
                 console.log(err);
                 return interaction.followUp('An error occured when applying pokemon to background, please log with bot author');
             });
-            
-            await guess.getBufferAsync(Jimp.MIME_PNG).then(async buffer => {
+
+            await guess[0].getBufferAsync(Jimp.MIME_PNG).then(async buffer => {
                 const finalImg = new MessageAttachment(buffer, `${name}.png`);
-                return await interaction.followUp({content: `Starting from now you have 2 minutes to guess this pokemon!`, files: [finalImg] });
+                return await interaction.followUp({ content: `Starting from now you have 2 minutes to guess this pokemon!`, files: [finalImg] });
             }).catch(err => {
                 console.log(err);
                 return interaction.followUp('An error occured when sending the final image, please log with the bot author');
             });
 
+            const filter = m => m.content.toLowerCase().includes(name.toLowerCase().split(' ').pop());
+
+            const collector = interaction.channel.createMessageCollector({ filter, max: 1, time: 120000 });
+
+            collector.on('collect', async message => {
+                return await message.reply(`Congratulations ${message.author}!!! You guessed correctly it was ${name}`);
+            });
+
+            collector.on('end', async collected => {
+                await guess[1].getBufferAsync(Jimp.MIME_PNG).then(async buffer => {
+                    const finalImg = new MessageAttachment(buffer, `${name}.png`);
+                    return await interaction.channel.send({ files: [finalImg] });
+                }).catch(err => {
+                    console.log(err);
+                    return interaction.followUp('An error occured when sending the true final image, please log with the bot author');
+                });
+
+                if (collected.size != 0) return;
+                return await interaction.followUp(`Tough luck no one guessed correctly it was ${name}`);
+            });
 
         } catch (error) {
             await interaction.reply('An error occured, which shouldnt be possible please let the bot author know!');
