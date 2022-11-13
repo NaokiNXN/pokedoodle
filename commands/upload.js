b0ba3ee8b87d753891d1d1d6707e5d502bd37acc
NaokiNXN <nxn1@outlook.com>
2022-11-13 22:28:18 +0200

    upload fix

--- commands/upload.js
+++ commands/upload.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { Interaction, Message } = require('discord.js');
+const { Interaction } = require('discord.js');
 const Jimp = require('jimp');
 
 /**
